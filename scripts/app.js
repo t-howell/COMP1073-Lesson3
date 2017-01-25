40dcbd12bb67e998e5327608e624fd97923d1442
thowell <tamara.howell@hotmail.com>
2017-01-25 13:17:55 -0500

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+/* JavaScript lives here */
+
+console.log("App Started");
+
+//create a reference to the button on the page (index.html)
+var clickMeButton = document.getElementById("clickMeButton");
+
+// add an event listener - for the click event and call the click function
+clickMeButton.addEventListener("click", clickMe);
+
+//click function - used as an event handler
+function clickMe(){
+	console.log("Clicked!");
+}
+
+/* var p = document.createElement("p");
+
+var snippet = p.createTextNode("This is a paragraph."); */
+
+targetArea.appendChild(snippet);
