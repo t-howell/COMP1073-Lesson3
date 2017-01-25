9dff9d07069269e39f9b884a8a7c8cad77f42bcb
thowell <tamara.howell@hotmail.com>
2017-01-25 14:07:09 -0500

    myContent

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,27 @@ console.log("App Started");
 var firstHeading = document.getElementById("firstHeading");
 
 //can use firstHeading js variable as though it is the html h1 element
-window.alert(firstHeading.textContent);
+console.log(firstHeading.textContent);
+
+firstHeading.textContent = "Bla bla bla";
+//this will destroy your document/overwrite it
+//document.write(firstHeading.textContent);
+
+//dont use this
+//window.alert(firstHeading.textContent);
+
+
+
+
+//*****For assignment 1 do these three steps
+//1. create reference to an element id (reference variable)
+var firstParagraph = document.getElementById("firstParagraph");
+
+//2. create a variable that contains content (content variable)
+var myContent = "It was a sunny day in Florida. Blah blah blah.";
+
+//3. assign variable with content to the reference variable ()
+firstParagraph.textContent = myContent;
 
 
 
